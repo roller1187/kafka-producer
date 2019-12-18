145c6a7ce523c8676fc6f434cdfc64a47f866a73
Andres Romero <aromero@redhat.com>
2019-12-18 12:13:31 -0500

    Programatically create keystore file

--- src/main/java/com/redhat/kafkaproducer/KafkaDemoApplication.java
+++ src/main/java/com/redhat/kafkaproducer/KafkaDemoApplication.java
@@ -1,5 +1,9 @@
 package com.redhat.kafkaproducer;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -11,7 +15,13 @@ public class KafkaDemoApplication implements CommandLineRunner {
 	
 	public static Logger logger = LoggerFactory.getLogger(KafkaDemoApplication.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws GeneralSecurityException, IOException {
+		Properties props = new Properties();
+		props.load(KafkaDemoApplication.class.getClassLoader().getResourceAsStream("application.properties"));
+		
+		TrustStore.createFromCrtFile(props.getProperty("kafka.cert.path"),
+				props.getProperty("spring.kafka.properties.ssl.truststore.location"),
+				props.getProperty("spring.kafka.properties.ssl.truststore.password").toCharArray());
 		SpringApplication.run(KafkaDemoApplication.class, args);
 	}
 
